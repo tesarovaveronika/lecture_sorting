1c6410b4d2623c02a84031ffce98d5f0e3103018
tesarovaveronika <269127@vut.cz>
2025-04-14 08:36:58 +0200

    info

--- sorting.py
+++ sorting.py
@@ -10,9 +10,30 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as file:
+        reader = csv.reader(file)
+        slovnik = {}
+        n=0
+        for row in reader:
+            if n == 0:
+                parts = row.split(",")
+                slovnik[parts[0]] = []
+                slovnik[parts[1]] = []
+                slovnik[parts[2]] = []
+            else:
+                casti = row.split(",")
+                slovnik[parts[0]].append(int(casti[0]))
+                slovnik[parts[1]].append(int(casti[1]))
+                slovnik[parts[2]].append(int(casti[2]))
+            n += 1
+    return slovnik
+
+
+
 
 
 def main():
+    slovnik = read_data(numbers.csv)
     pass
 
 
